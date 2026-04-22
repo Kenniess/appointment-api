3f6a252938e05a8c101dff7655f1d4c77eefb081
Kenniess <nken2547@gmail.com>
2026-04-22 22:57:53 +0700

    playwright

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000"
+                   https://cn334-frontend1.vercel.app/],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
